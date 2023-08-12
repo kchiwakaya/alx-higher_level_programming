25f743597ea034b571335b913e2353adec172b4d
Kevin Chiwakaya <40562667+kchiwakaya@users.noreply.github.com>
2023-08-12 02:30:13 +0200

    Update 100-my_calculator.py

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,25 +1,23 @@
 #!/usr/bin/python3
 from sys import argv
 from calculator_1 import add, sub, mul, div
-if __name__ != "__main__":
-    exit()
+if __name__ == "__main__":
+    args = len(argv) - 1
+    if args != 3:
+        print("Usage: {:s} <a> <operator> <b>".format(argv[0]))
+        exit(1)
+    elif argv[2] == '+':
+        function = add
+    elif argv[2] == '-':
+        function = sub
+    elif argv[2] == '*':
+        function = mul
+    elif argv[2] == '/':
+        function = div
+    else:
+        print("Unknown operator. Available operators: +, -, *, and /")
+        exit(1)
 
-argc = len(argv) - 1
-if argc != 3:
-    print("Usage: {:s} <a> <operator> <b>".format(argv[0]))
-    exit(1)
-elif argv[2] == '+':
-    function = add
-elif argv[2] == '-':
-    function = sub
-elif argv[2] == '*':
-    function = mul
-elif argv[2] == '/':
-    function = div
-else:
-    print("Unknown operator. Available operators: +, -, *, and /")
-    exit(1)
-
-output = function(int(argv[1]), int(argv[3]))
-print("{:d} {:s} {:d} = {:d}".format(int(argv[1]),
+    output = function(int(argv[1]), int(argv[3]))
+    print("{:d} {:s} {:d} = {:d}".format(int(argv[1]),
     argv[2], int(argv[3]), output))
