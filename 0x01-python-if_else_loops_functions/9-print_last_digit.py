6baf66e07f8f926edcb55973b79e564ef3e98316
kchiwakaya <kchiwakaya@gmail.com>
2023-08-18 17:23:08 +0200

    pyfun

---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def print_last_digit(number):
+    temp = 0
+    if (number < 0):
+        temp = -(number)
+    else:
+        temp = number
+    print (temp % 10,end="")
+    return temp % 10
