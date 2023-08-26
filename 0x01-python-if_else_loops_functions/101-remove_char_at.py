197b26db1ddfa05d78bdf821b1e901948f5a5c70
kchiwakaya <kchiwakaya@gmail.com>
2023-08-26 14:45:11 +0200

    remove

---
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def remove_char_at(str, n):
+    
+    theString = ""
+    a = 0
+ 
+    for character in str:
+        if a != n:
+            theString += character
+        a += 1
+    return (theString)
+
