2d6c3555b8d6f527dff32893bf25fd67272c178a
kchiwakaya <kchiwakaya@gmail.com>
2023-08-28 21:49:10 +0200

    bare spa

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -3,6 +3,6 @@ def safe_print_integer(value):
     try:
         print("{:d}".format(value))
         isInt = True
-    except:
+    except (ValueError, TypeError):
         isInt = False
     return isInt
