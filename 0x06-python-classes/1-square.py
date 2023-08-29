c4281008ea695ce05952498fc903093658560685
kchiwakaya <kchiwakaya@gmail.com>
2023-08-29 16:05:32 -0700

    tail

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+"""Class sqaure"""
 class Square:
     """
     Square class with attributes :
--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+"""Define class square"""
 class Square:
     """
     Square class with attributes:
