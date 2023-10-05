ea712eb56a8ec79e2bfcec286043c0bd47b42086
Kevin Chiwakaya <40562667+kchiwakaya@users.noreply.github.com>
2023-10-05 17:19:19 +0200

    Update 6-max_integer_test.py

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""unit testing module for 6-max_integer_test.py
+"""
+unit tests for 6-max_integer_test.py
 """
 import unittest
 max_int = __import__('6-max_integer').max_integer
@@ -7,22 +8,25 @@ max_int = __import__('6-max_integer').max_integer
 
 class TestMaxInteger(unittest.TestCase):
     """
-    class for testing 6-max_integer_test.py
+    testing 6-max_integer_test.py
     """
 
     def test_max_integer(self):
-        """ test case for normal list of integers w/o negatives
+        """ 
+        test case for normal list of integers
         """
         test_list = [1, 2, 3, 8, 4]
         self.assertEqual(max_int(test_list), 8)
 
     def test_max_integer_neg(self):
-        """ test case for normal list of integers w/ negatives
+        """ 
+        test case for normal list of integers
         """
         test_list = [1, 2, 3, 8, 4, -40, -400, -12, 0]
         self.assertEqual(max_int(test_list), 8)
 
     def test_max_integer_(self):
-        """ test case for empty list
+        """
+        test case for empty list
         """
         self.assertEqual(max_int([]), None)
