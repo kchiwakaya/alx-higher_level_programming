0b451329889eb59e0098ce4b401c3dcaa114f300
kchiwakaya <kchiwakaya@gmail.com>
2023-12-11 14:54:55 +0200

    kkk

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+const process = require('process');
+if (process.argv.length === 3)
+{
+  console.log('Argument found');
+} else if (process.argv.length < 3) 
+{
+  console.log('No argument');
+} else
+{
+  console.log('Arguments found');
+}
+
