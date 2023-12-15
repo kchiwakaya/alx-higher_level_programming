1d92c9efb9a3ee88ad6fc0bb4f0a6507b72acfbf
kchiwakaya <kchiwakaya@gmail.com>
2023-12-15 09:20:29 +0200

    python stuff

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" Script to print all states"""
+import sys
+import MySQLdb
+
+if __name == '__main__':
+    """getting an array of passed arguments"""
+    args = sys.argv
+    """ handle inadequate arguments"""
+    if len(args) < 4:
+        exit(0)
+    username = args[1]
+    password = args[2]
+    db = args[3]
+
+    db = MySQLdb.connect(host ='localhost',user = username,
+            password = password, db = db,
+            port = 3306)
+
+    cursor = db.cursor()
+    query = cursor.execute("SELECT * FROM BY states.id")
+    records = cursor.fetchall()
+    for row in records:
+        print(row)
